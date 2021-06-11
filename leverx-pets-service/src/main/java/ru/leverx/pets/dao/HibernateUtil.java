b482ad0ceda42e0d5e6ccaf3ba8376a25b74df88
Valery1508 <lera.zubritskaya@gmail.com>
2021-06-11 16:13:52 +0300

    Connected persistence.xml

--- leverx-pets-service/src/main/java/ru/leverx/pets/dao/HibernateUtil.java
+++ leverx-pets-service/src/main/java/ru/leverx/pets/dao/HibernateUtil.java
@@ -2,59 +2,31 @@ package ru.leverx.pets.dao;
 
 import org.hibernate.SessionException;
 import org.hibernate.SessionFactory;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.hibernate.cfg.Configuration;
-import org.hibernate.cfg.Environment;
-import org.hibernate.service.ServiceRegistry;
-import ru.leverx.pets.entity.Person;
-import ru.leverx.pets.entity.Pet;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 import java.util.Objects;
-import java.util.Properties;
 
-import static ru.leverx.pets.Constants.*;
+import static ru.leverx.pets.exception.ExceptionMessages.SESSION_MESSAGE;
 
 public class HibernateUtil {
     private static SessionFactory sessionFactory;
 
-    public static SessionFactory getSessionFactory() throws IOException {
-        Properties properties = new Properties();
-        InputStream inputStream = HibernateUtil.class.getClassLoader().getResourceAsStream(PROPERTIES_FILE);
-        if (inputStream != null) {
-            properties.load(inputStream);
-        } else {
-            throw new FileNotFoundException(FILE_NOT_FOUND_MESSAGE);
-        }
+    public static SessionFactory getSessionFactory() {
 
         if (Objects.isNull(sessionFactory)) {
             try {
-                Configuration configuration = new Configuration();
-
-                Properties settings = new Properties();
-                settings.put(Environment.DRIVER, properties.getProperty("db.driver"));
-                settings.put(Environment.URL, properties.getProperty("db.url"));
-                settings.put(Environment.USER, properties.getProperty("db.user"));
-                settings.put(Environment.PASS, properties.getProperty("db.password"));
-                settings.put(Environment.DIALECT, properties.getProperty("db.dialect"));
-                settings.put(Environment.SHOW_SQL, properties.getProperty("db.show_sql"));
-                settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, properties.getProperty("db.current_session_context_class"));
-                settings.put(Environment.HBM2DDL_AUTO, properties.getProperty("db.hbm2ddl_auto"));
-
-                configuration.setProperties(settings);
-                configuration.addAnnotatedClass(Pet.class);
-                configuration.addAnnotatedClass(Person.class);
-
-                ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
-                        .applySettings(configuration.getProperties()).build();
-                sessionFactory = configuration.buildSessionFactory(serviceRegistry);
-                return sessionFactory;
+                return loadSessionFactory();
             } catch (Exception e) {
                 throw new SessionException(SESSION_MESSAGE);
             }
         }
         return sessionFactory;
+    }
+
+    public static SessionFactory loadSessionFactory() {
+        EntityManagerFactory entityManagerFactory = Persistence
+                .createEntityManagerFactory("persistenceUnit");
+        return entityManagerFactory.unwrap(SessionFactory.class);
     }
 }
